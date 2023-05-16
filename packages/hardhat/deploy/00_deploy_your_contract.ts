75486ebb449d85c823afd80d1a32382d240b2245
solidoracle <solidoracle17@gmail.com>
2023-05-16 09:58:13 +0200

    goerli deploy

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -2,12 +2,12 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 
 /**
- * Deploys a contract named "YourContract" using the deployer account and
+ * Deploys a contract named "SolidVault" using the deployer account and
  * constructor arguments set to the deployer address
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deploySolidVault: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
     On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
 
@@ -20,11 +20,14 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   */
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
+  const wethAddress = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6";
+  const aaveLendingPoolAddress = "0x73D94B5D5C0a68Fe279a91b23D2165D2DAaA41d3";
+  const aaveRewardsAddress = "0xCCB14936C2E000ED8393A571D15A2672537838Ad";
 
-  await deploy("YourContract", {
+  await deploy("SolidVault", {
     from: deployer,
     // Contract constructor arguments
-    args: [deployer],
+    args: [wethAddress, deployer, aaveLendingPoolAddress, aaveRewardsAddress],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
@@ -32,11 +35,11 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   });
 
   // Get the deployed contract
-  // const yourContract = await hre.ethers.getContract("YourContract", deployer);
+  await hre.ethers.getContract("SolidVault", deployer);
 };
 
-export default deployYourContract;
+export default deploySolidVault;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
-// e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+// e.g. yarn deploy --tags SolidVault
+deploySolidVault.tags = ["SolidVault"];
